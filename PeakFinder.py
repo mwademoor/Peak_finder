39985604591931dc36716a69e5c67aea637f670d
Suzanne van Dam <suzannevandam@gmailcom>
2015-11-04 11:05:26 +0000

    I added the empty PeakfinderNotebook, and Peakfinder.py with a peakFinder class in it

---
+++ PeakFinder.py
@@ -0,0 +1,12 @@
+"""Peak Finder module, for the Casimir course. authors: Suzanne van Dam and Michiel de Moor"""
+<<<<<<< HEAD
+
+class PeakFinder(object):
+    """the peakfinder class"""
+    
+    def __init__(data_file):
+        self.f = file
+
+def import_data_from_file(file)
+=======
+>>>>>>> 022910064e4b3f610b5b79209b02d6ac5874b300
